f9e86c08b5f780ab06b72fff3f6fa6313a67e882
Vitalij <vitalij.cernysiov@gmail.com>
2022-07-14 17:29:50 +0300

    class

--- JS/class.js
+++ JS/class.js
@@ -1,1 +1,22 @@
 console.clear();
+
+class Car {
+    constructor (pavadi, spalva) {
+        this.name = pavadi;
+        this.age = 10;
+        this.spalva = spalva;
+    }
+
+    introduce () {
+        return `Im ${this.name} the besT car`;
+    }
+
+}
+
+
+const BMW = new Car('BMW', "juoda");
+const WV = new Car('WV', "melyna");
+BMW.age = 12;
+
+console.log(BMW);
+console.log(WV);
