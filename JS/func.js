f910f1aa1165b8411a13ec7dab26f4520f5fca70
Vitalij <vitalij.cernysiov@gmail.com>
2022-12-19 19:10:20 +0200

    codewars exercise

--- JS/func.js
+++ JS/func.js
@@ -84,8 +84,8 @@ console.clear()
 
 function hasSurvived(attackers, defenders){
 
-    const sum = attackers.reduce((accumulator, value) => {
-        return accumulator + value;
+    const sum = attackers.reduce((accumulator, n) => {
+        return accumulator + n;
       }, 0);
 
       const suma = defenders.reduce((accum, val) => {
@@ -203,10 +203,10 @@ console.clear();
 
 // Deadfish has 4 commands, each 1 character long:
 
-// i increments the value (initially 0)
-// d decrements the value
-// s squares the value
-// o outputs the value into the return array
+// i increments the n (initially 0)
+// d decrements the n
+// s squares the n
+// o outputs the n into the return array
 // Invalid characters should be ignored.
 
 // parse("iiisdoso") => [ 8, 64 ]
@@ -321,5 +321,93 @@ console.log(strictEqual(10))
 
 // console.clear();
 
+// function SubtractSum(n){
+//   let sum = 0;
+//   let sap = 'cherry';
+//   let pat = n;
+//   let del = 0
+//    while (del > 101) {
+//     sum += pat % 10;
+//     pat = Math.floor(n / 10);
+//     del = pat - sum;
+//   }  
+// if (del == 1 || del == 3 || del == 24 || del == 26 || del == 47 || del == 49 || del ==68 || del == 70 || del == 91 || del == 93) {sap = 'kiwi'}
+// else if (del == 2 || del == 21 || del == 23 || del == 42 || del == 44 || del == 46 || del == 65 || del == 67 || del == 69 || del == 88) {sap = 'pear'}
+// else if (del == 9 || del == 18 || del == 27 || del == 36 || del == 45 || del == 54 || del == 63 || del == 72 || del == 81 || del == 90) {sap = 'apple'}
+// else if (del == 4 || del == 6 || del == 25 || del == 29 || del == 48 || del == 50 || del == 71 || del == 73 || del == 92 || del == 94 ) {sap = 'banana'}
+// else if (del == 5 || del == 7 || del == 28 || del == 30 || del == 32 || del == 51 || del == 53 || del == 74 || del == 76 || del == 95 || del == 97) {sap = 'melon'}
+// else if (del == 8 || del == 10 || del == 12 || del == 31 || del == 33 || del == 52 || del == 56 || del == 75 || del == 77 || del == 79 || del == 98 || del == 100) {sap = 'pineapple'}
+// else if (del == 11 || del == 13 || del == 34 || del == 55 || del == 57 || del == 59 || del == 78 || del == 80 ) {sap = 'cucumber'}
+// else if (del == 14 || del == 16 || del == 35 || del == 37 || del == 39 || del == 58 || del == 60 || del == 83 ) {sap = 'orange'}
+// else if (del == 86 || del == 84 || del == 82 || del == 61 || del == 40 || del == 38 || del == 19 || del == 17 || del == 15) {sap = 'grape'}
+// // else if {sap = 'cherry'}
+// return del
+// }
 
+// console.log(SubtractSum(150))
 
+function SubtractSum(n){
+  let sum = 0;
+  let sap = 'cherry';
+  pat = n;
+  let del = 0
+  while (pat) {
+    sum += pat % 10;
+    pat = Math.floor(pat / 10);
+ }
+    do {
+    del = n - sum;
+  }
+  while (del > 101) {
+}
+if (del == 1 || del == 3 || del == 24 || del == 26 || del == 47 || del == 49 || del ==68 || del == 70 || del == 91 || del == 93) {sap = 'kiwi'}
+else if (del == 2 || del == 21 || del == 23 || del == 42 || del == 44 || del == 46 || del == 65 || del == 67 || del == 69 || del == 88) {sap = 'pear'}
+else if (del == 9 || del == 18 || del == 27 || del == 36 || del == 45 || del == 54 || del == 63 || del == 72 || del == 81 || del == 90) {sap = 'apple'}
+else if (del == 4 || del == 6 || del == 25 || del == 29 || del == 48 || del == 50 || del == 71 || del == 73 || del == 92 || del == 94 ) {sap = 'banana'}
+else if (del == 5 || del == 7 || del == 28 || del == 30 || del == 32 || del == 51 || del == 53 || del == 74 || del == 76 || del == 95 || del == 97) {sap = 'melon'}
+else if (del == 8 || del == 10 || del == 12 || del == 31 || del == 33 || del == 52 || del == 56 || del == 75 || del == 77 || del == 79 || del == 98 || del == 100) {sap = 'pineapple'}
+else if (del == 11 || del == 13 || del == 34 || del == 55 || del == 57 || del == 59 || del == 78 || del == 80 ) {sap = 'cucumber'}
+else if (del == 14 || del == 16 || del == 35 || del == 37 || del == 39 || del == 58 || del == 60 || del == 83 ) {sap = 'orange'}
+else if (del == 86 || del == 84 || del == 82 || del == 61 || del == 40 || del == 38 || del == 19 || del == 17 || del == 15) {sap = 'grape'}
+// else if {sap = 'cherry'}
+return sap
+}
+
+console.log(SubtractSum(44))
+
+console.clear()
+
+function dontGiveMeFive(start, end)
+{
+  let penki = 0;
+  let oplia = 0;
+  let pp = 0;
+  let stop = 0;
+  let kadr = 0;
+  let dif = 0
+  for (i = start; i <= end; i++) {
+  if (i > 0) (dif++);
+  if (i % 5 == 1 && i % 10 != 1) (penki++);
+  // if (i == 5,15,25,35,45,65,75,85,95,105) (penki++);
+  if (i > 49 && i < 60 || i > 499 && i < 600) (stop++);
+  if (i == 55) {kadr = 1};}
+  oplia = Math.ceil(penki/2);
+  //  if (start < 2) {oplia = oplia - 1}
+  pp = dif - penki - stop - kadr
+ 
+  return pp
+}
+
+console.log(dontGiveMeFive(2,615))
+
+function dontGiveMeFive(start, end) {
+  let count = 0
+  for (let i = start; i <= end; i++) {
+    if (!/5/.test(i)) {
+      count++
+    }
+  }
+  return count
+}
+
+console.log(dontGiveMeFive(2,615))
