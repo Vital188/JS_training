f8724988c1a27e1416c1c399a6a20cdeb9af0396
Vitalij <vitalij.cernysiov@gmail.com>
2023-02-04 13:59:24 +0200

    update

--- JS/practise.js
+++ JS/practise.js
@@ -1,3 +1,12 @@
-function isPangram(string){
-    //...
-  }
+function Hero (name) {
+let name = {
+    name: 'Hero',
+    position: '00',
+    health: 100,
+    damage: 5,
+    experience: 0
+  };
+  return `Hero should have a ${Hero.key} attribute with value ${Hero.val}`
+}
+
+console.log((name.position))
