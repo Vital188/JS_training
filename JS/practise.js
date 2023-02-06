ce0770be0499cc53000f29909634fc8b93ea3e4c
Vitalij <vitalij.cernysiov@gmail.com>
2023-02-06 21:22:17 +0200

    codewars exercise

--- JS/practise.js
+++ JS/practise.js
@@ -12,3 +12,17 @@ function race(v1, v2, g) {
   let time = [];
 if (v1 >= v2) {tiem = null} 
 }
+
+console.clear();
+
+function hammingDistance (a, b) {
+	arra = Array.from(a);
+  arrb = Array.from(b);
+  let result = 0;
+  for (let i = 0; i <= arra.length; i++) {
+    if (arra[i] != arrb[i]) {result++}
+  }
+  return result
+}
+
+console.log(hammingDistance('', ''))
