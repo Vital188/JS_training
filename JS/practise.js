7735339d6f91f47f09c1594d45b68bde7845d0c9
Vitalij <vitalij.cernysiov@gmail.com>
2023-02-15 16:08:30 +0200

    codewars exercise

--- JS/practise.js
+++ JS/practise.js
@@ -43,5 +43,22 @@ console.log(solve("$Cnl)Sr<7bBW-&qLHI!mY41ODe"))
 console.clear();
 
 function sumOfMinimums(arr) {
-  // your code here
-}
+  let minvalue = [];
+  let sum = 0;
+  let sortarr = [];
+ for (let i = 0; i <arr.length; i++) {
+    sortarr[i] = arr[i].sort(function(a, b){return a-b});
+    minvalue[i] = sortarr[i][0];
+ } 
+ for (let j = 0; j < minvalue.length; j++) {
+  sum += minvalue[j];
+ }
+ return sum
+ } 
+  
+
+
+
+console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]))
+
+console.clear();
